ef6315a10f97ec28746d2a91748d4cccc459ac64
whsgzcy <whsgzcy@foxmail.com>
2017-07-17 21:31:10 +0800

    Initial Commit

---
+++ ioser/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ioser
+//
+//  Created by 郑传余 on 2017/7/17.
+//  Copyright © 2017年 郑传余. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ioser/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ioser
+//
+//  Created by 郑传余 on 2017/7/17.
+//  Copyright © 2017年 郑传余. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
